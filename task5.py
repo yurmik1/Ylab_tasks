d255a7dbf3c105981b3d17beb812d8bc7a59b3c3
yurmik1 <101806547+yurmik1@users.noreply.github.com>
2022-06-24 20:15:02 +0300

    Add files via upload

--- task5.py
+++ task5.py
@@ -1,22 +1,19 @@
 def count_find_num(primesL, limit):
     primesL = set(primesL)
-    numbers = []
-    for i in range(limit+1):
-        cur_primesL = set()
+    numbers = set()
+    for i in range(2, limit + 1):
+        cur_primes_l = set()
         cur_number = i
-        d = 2
-        while d * d <= i:
-            if i % d == 0:
-                cur_primesL.add(d)
-                i //= d
-            else:
-                d += 1
+        for j in primesL:
+
+            while i % j == 0:
+                cur_primes_l.add(j)
+                i = i / j
         if i > 1:
-            cur_primesL.add(i)
-        if cur_primesL == primesL:
-            numbers.append(cur_number)
-    res = [len(numbers), max(numbers)] if numbers != [] else []
-    return res
+            cur_primes_l.add(i)
 
+        if cur_primes_l == primesL:
+            numbers.add(cur_number)
 
+    return [len(numbers), max(numbers)] if numbers != set() else []
 
