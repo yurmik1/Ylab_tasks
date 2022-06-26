8db058ffbb68b5e231f43d571d5ab0e45d81b09c
yurmik1 <101806547+yurmik1@users.noreply.github.com>
2022-06-26 15:43:51 +0300

    Add files via upload

--- task5.py
+++ task5.py
@@ -1,19 +1,28 @@
+import math
+import sys
+from itertools import combinations_with_replacement
+
+
 def count_find_num(primesL, limit):
-    primesL = set(primesL)
-    numbers = set()
-    for i in range(2, limit + 1):
-        cur_primes_l = set()
-        cur_number = i
-        for j in primesL:
+    if math.prod(primesL) > limit:
+        return []
+    rates = ()
+    n = ()
 
-            while i % j == 0:
-                cur_primes_l.add(j)
-                i = i / j
-        if i > 1:
-            cur_primes_l.add(i)
+    for i in primesL:
+        rate = 2
+        while i <= limit:
+            i = i**rate
+            rate = rate + 1
+        rates += (rate,)
+    print(sum(rates))
+    for i in range(len(primesL), sum(rates)+sum(rates) //2):
+        n += tuple(combinations_with_replacement(primesL, i))
+    s = list(n)
+    print(sys.getsizeof(s))
+    print(sys.getsizeof(n))
 
-        if cur_primes_l == primesL:
-            numbers.add(cur_number)
-
-    return [len(numbers), max(numbers)] if numbers != set() else []
+    n_edit = tuple(i for i in n if set(primesL) == set(i))
+    numbers = tuple(math.prod(i) for i in n_edit if math.prod(i) <= limit)
+    return [len(numbers), max(numbers)]
 
