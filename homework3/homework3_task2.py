e6149ef3db1a31319378902d780b6853977c68c7
yurmik1 <101806547+yurmik1@users.noreply.github.com>
2022-07-12 18:17:53 +0300

    Add files via upload

--- homework3/homework3_task2.py
+++ homework3/homework3_task2.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
             def wrapper():
                 print(f'Кол-во запусков = {call_count}')
                 print('Начало работы')
-                t = start_sleep_time * factor ** 1
+                t = start_sleep_time * factor ** 0
                 for i in range(1, call_count + 1):
                     time.sleep(t)
                     func_result = func()
@@ -18,14 +18,14 @@ if __name__ == '__main__':
                     else:
                         sec = "секунд"
                     print(f'Запуск номер {i}. Ожидание: {t} {sec}. Результат декорируемой функций = {func_result}')
-                    t = start_sleep_time * factor**(i+1) if start_sleep_time * factor**(i+1)\
+                    t = start_sleep_time * factor**(i) if start_sleep_time * factor**(i)\
                                                             < border_sleep_time else border_sleep_time
                 print('Конец работы')
             return wrapper
         return func_decorator
 
 
-    @df_decorator(call_count=10, start_sleep_time=1, factor=2, border_sleep_time=60)
+    @df_decorator(call_count=5, start_sleep_time=1, factor=2, border_sleep_time=20)
     def function():
         return 'Функция выполнена'
 
